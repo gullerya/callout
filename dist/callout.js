2fbd44eac628b777a9f1e1b3a158c6e4966eed0f
gullerya <gullerya@gmail.com>
2019-09-03 11:37:32 +0300

    wip

--- dist/callout.js
+++ dist/callout.js
@@ -14,8 +14,6 @@ function callout(elements) {
 
 		const r = getScreenRect(e);
 		putCalloutVilon(r);
-
-		//	place the rest of the management elements over the mask
 	}
 }
 
@@ -28,14 +26,75 @@ function getScreenRect(e) {
 }
 
 function putCalloutVilon(r) {
-	//	if the vilon is already shown - animate, otherwise - create
-	// <svg>
-	// 	<defs>
-	// 		<mask id="mask">
-	// 			<rect x="0" y="0" width="100%" height="100%" fill="#aaa" opacity="0.9" />
-	// 			<rect x="50" y="50" width="100" height="100" rx="12px" />
-	// 		</mask>
-	// 	</defs>
-	// 	<rect x="0" y="0" width="100%" height="100%" mask="url(#mask)" />
-	// </svg>
+	const calloutElement = document.createElement('gullerya-callout');
+	document.body.appendChild(calloutElement);
 }
+
+const template = document.createElement('template');
+
+template.innerHTML = `
+	<style>
+		:host {
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			z-index: 1000;
+		}
+
+		.man-pan {
+			position: absolute;
+			top: 50px;
+			left: 500px;
+			color: #000;
+		}
+
+		.callout-vilon {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+
+		.mask-overlay {
+			x: 0;
+			y: 0;
+			width: 100%;
+			height: 100%;
+			fill: #aaa;
+			opacity: 0.7;
+		}
+
+		.mask-window {
+			x: calc(50% - 12px);
+			y: calc(50% - 12px);
+			width: 24px;
+			height: 24px;
+			rx: 8px;
+			transition: all 200ms;
+		}
+	</style>
+
+	<svg class="callout-vilon">
+		<defs>
+			<mask id="callout-mask">
+				<rect class="mask-overlay"/>
+				<rect class="mask-window"/>
+			</mask>
+		</defs>
+		<rect x="0" y="0" width="100%" height="100%" mask="url(#callout-mask)"/>
+	</svg>
+	<div class="man-pan">
+		Some text here
+	</div>
+`;
+
+customElements.define('gullerya-callout', class extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' })
+			.appendChild(template.content.cloneNode(true));
+	}
+});
