a94fae0ceb6ea979df21173bb75ab600687db8cb
gullerya <gullerya@gmail.com>
2019-09-20 09:36:50 +0300

    wip

--- build/tools/build-dist.js
+++ build/tools/build-dist.js
@@ -2,7 +2,10 @@
 	os = require('os'),
 	fs = require('fs'),
 	fsExtra = require('fs-extra'),
-	uglifyES = require('uglify-es');
+	uglifyES = require('uglify-es'),
+	minifyOptions = {
+		toplevel: true
+	};
 
 process.stdout.write('cleaning "dist"...');
 fsExtra.emptyDirSync('./dist');
@@ -13,11 +16,8 @@ fsExtra.copySync('./src', './dist');
 process.stdout.write('\t\x1B[32mOK\x1B[0m' + os.EOL);
 
 process.stdout.write('minifying...');
-let options = {
-	toplevel: true
-};
 fs.writeFileSync(
 	'./dist/callout.min.js',
-	uglifyES.minify(fs.readFileSync('./dist/callout.js', { encoding: 'utf8' }), options).code
+	uglifyES.minify(fs.readFileSync('./dist/callout.js', { encoding: 'utf8' }), minifyOptions).code
 );
 process.stdout.write('\t\t\t\x1B[32mOK\x1B[0m' + os.EOL);
