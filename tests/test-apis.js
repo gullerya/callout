300aa49e44abdd5b7135bb4cb31febdc888777ee
gullerya <gullerya@gmail.com>
2020-02-05 22:16:17 +0200

    updated dependencies, added funding info

--- tests/test-apis.js
+++ tests/test-apis.js
@@ -1,7 +1,7 @@
-﻿import { createSuite } from '../../node_modules/just-test/dist/just-test.min.js'
+﻿import { getSuite } from '../../node_modules/just-test/dist/just-test.min.js'
 import { callout, SHAPES } from '../../dist/callout.js';
 
-const suite = createSuite({ name: 'Testing callout APIs' });
+const suite = getSuite({ name: 'Testing callout APIs' });
 
 suite.runTest({ name: 'test A' }, async test => {
 	const
